a21175e0dcfbe8ac6c4394a3c00e3ab4993dc58f
Patrick Meenan <pmeenan@chromium.org>
2018-04-03 13:35:40 -0400

    Added a done message

--- gce_image.sh
+++ gce_image.sh
@@ -20,3 +20,5 @@ gcloud compute --project=$PROJECT_ID instance-templates create $NAME \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \ 
     --min-cpu-platform=Automatic --image=wpt-linux-20180313 --image-project=webpagetest-official --boot-disk-size=10GB \
     --boot-disk-type=pd-standard --boot-disk-device-name=$NAME
+
+echo "Done."
