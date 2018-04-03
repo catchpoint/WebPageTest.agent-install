ab73092061af6515dd8d47e61cd4a7851264ded0
Patrick Meenan <pmeenan@chromium.org>
2018-04-03 13:55:50 -0400

    Fixed a stray space at the end of a line

--- gce_image.sh
+++ gce_image.sh
@@ -15,7 +15,7 @@ gcloud compute --project=${PROJECT_ID} instance-templates create ${TEMPLATE_NAME
     --machine-type=n1-standard-2 --network=projects/${PROJECT_ID}/global/networks/default \
     --metadata=\"${INSTANCE_METADATA}\" \
     --no-restart-on-failure --maintenance-policy=TERMINATE --preemptible --service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
-    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \ 
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
     --min-cpu-platform=Automatic --image=wpt-linux-20180313 --image-project=webpagetest-official --boot-disk-size=10GB \
     --boot-disk-type=pd-standard --boot-disk-device-name=${TEMPLATE_NAME}
 
