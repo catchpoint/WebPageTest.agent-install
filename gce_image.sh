fd3ffcffbe6f7596ccf723d05413b7c8e7facd9b
Patrick Meenan <pmeenan@chromium.org>
2018-04-03 14:22:08 -0400

    More bash foo

--- gce_image.sh
+++ gce_image.sh
@@ -12,12 +12,8 @@ ESCAPED_METADATA=$(printf %q "$INSTANCE_METADATA")
 PROJECT_ID=$(curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
 PROJECT_NUMBER=$(curl "http://metadata.google.internal/computeMetadata/v1/project/numeric-project-id" -H "Metadata-Flavor: Google")
 
-gcloud compute --project=${PROJECT_ID} instance-templates create ${TEMPLATE_NAME} \
-    --machine-type=n1-standard-2 --network=projects/${PROJECT_ID}/global/networks/default \
-    --metadata=${ESCAPED_METADATA} \
-    --no-restart-on-failure --maintenance-policy=TERMINATE --preemptible --service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
-    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
-    --min-cpu-platform=Automatic --image=wpt-linux-20180313 --image-project=webpagetest-official --boot-disk-size=10GB \
-    --boot-disk-type=pd-standard --boot-disk-device-name=${TEMPLATE_NAME}
+COMMAND="gcloud compute --project=${PROJECT_ID} instance-templates create ${TEMPLATE_NAME} --machine-type=n1-standard-2 --network=projects/${PROJECT_ID}/global/networks/default --metadata=${ESCAPED_METADATA} --no-restart-on-failure --maintenance-policy=TERMINATE --preemptible --service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --min-cpu-platform=Automatic --image=wpt-linux-20180313 --image-project=webpagetest-official --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=${TEMPLATE_NAME}"
+echo $COMMAND
+eval $COMMAND
 
 echo "Done."
