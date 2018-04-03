279d63fe4a1aaa22f4184c92fbc43844dccc88f0
Patrick Meenan <pmeenan@chromium.org>
2018-04-03 13:35:05 -0400

    Initial stab at a console shell for using the public image

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wptagent-install
-Automated scripts for installing dedicated wptagent instances
+Automated scripts for installing dedicated wptagent agents
 
 ## Ubuntu 16.04:
 
@@ -24,3 +24,14 @@ wget https://raw.githubusercontent.com/WPO-Foundation/wptagent-install/master/ec
 chmod +x ec2_ubuntu.sh && \
 ./ec2_ubuntu.sh
 ```
+
+## Google Cloud Shared Image
+Open the cloud shell for the project where the agents will run and paste:
+
+```bash
+wget https://raw.githubusercontent.com/WPO-Foundation/wptagent-install/master/gce_image.sh && \
+chmod +x gce_image.sh && \
+./gce_image.sh
+```
+
+This will create an instance template in the project that can be used to create Managed Instance Groups or individual instances in any region.
---
+++ gce_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Prompt for the configuration options
+echo "WebPageTest instance template creator."
+
+read -p "Instance template name (defaults to wpt-agent): " NAME
+if [ $NAME != '' ]; then
+    NAME = 'wpt-agent'
+while [[ $METADATA == '' ]]
+do
+    read -p "Instance Metadata String (wpt_server=...): " METADATA
+done
+PROJECT_ID=$(curl "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+PROJECT_NUMBER=$(curl "http://metadata.google.internal/computeMetadata/v1/project/numeric-project-id" -H "Metadata-Flavor: Google")
+
+gcloud compute --project=$PROJECT_ID instance-templates create $NAME \
+    --machine-type=n1-standard-2 --network=projects/$PROJECT_ID/global/networks/default \
+    --metadata="$METADATA" \
+    --no-restart-on-failure --maintenance-policy=TERMINATE --preemptible --service-account=$PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \ 
+    --min-cpu-platform=Automatic --image=wpt-linux-20180313 --image-project=webpagetest-official --boot-disk-size=10GB \
+    --boot-disk-type=pd-standard --boot-disk-device-name=$NAME
