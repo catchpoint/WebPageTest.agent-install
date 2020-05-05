758f23d32dc5b708bee41082a696811f24add0c7
Jumpei Ogawa <phanective@gmail.com>
2020-05-06 02:22:12 +0900

    Add --system to sysctl instead of --load
    --load only reload config from /etc/sysctl.conf.
    --system also reload configs from other locations including 
    /etc/sysctl.d/*.conf.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -17,7 +17,7 @@ test -f "${HOME}/agent.sh"
 test "$(sudo cat /etc/sudoers.d/wptagent)" == "${USER} ALL=(ALL:ALL) NOPASSWD:ALL"
 test "$(cat /etc/watchdog.conf | grep "test-binary = $HOME/wptagent/alive.sh")" == "test-binary = $HOME/wptagent/alive.sh"
 
-sudo sysctl --load
+sudo sysctl --system
 
 test "$(sysctl vm.panic_on_oom)" == "vm.panic_on_oom = 1"
 test "$(sysctl kernel.panic)" == "kernel.panic = 10"
