fe987a23b6724c2e7bae230d796ee6e0139cb959
Patrick Meenan <PatMeenan@gmail.com>
2020-10-02 12:05:55 -0700

    Fixed Firefox install

--- debian.sh
+++ debian.sh
@@ -320,8 +320,8 @@ if [ "${AGENT_MODE,,}" == 'desktop' ]; then
         fi
 
         if [ "${WPT_FIREFOX,,}" == 'y' ]; then
-            wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-            sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+            sudo add-apt-repository -y ppa:ubuntu-mozilla-daily/ppa
+            sudo add-apt-repository -y ppa:mozillateam/ppa
             until sudo apt -y update
             do
                 sleep 1
